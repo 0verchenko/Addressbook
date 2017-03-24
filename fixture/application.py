01e3af5450194b259d323ce0eb1ac092961d5e9e
0verchenko <v.overchenko@gmail.com>
2017-03-24 10:43:43 +0200

    Add new files to repo

---
+++ fixture/application.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
+
+class Application:
+
+    def __init__(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+
+    def home_page(self):
+        wd = self.wd
+        wd.get("http://94.125.123.247/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
