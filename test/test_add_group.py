961d7d732d6ea7ec67da3e4a86f9413eb326a3c3
0verchenko <v.overchenko@gmail.com>
2017-06-09 08:35:28 +0300

    Add allure steps

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import pytest
 
 def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
+    with pytest.allure.step('Take existing group list from db'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('Create new group'):
+        app.group.create(group)
+    with pytest.allure.step('Take existing group list from db where we add groups'):
+        new_groups = db.get_group_list()
     old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step('Compare groups lists'):
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
